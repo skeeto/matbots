0ee8262a12d4481edffcc895f911d479dedbb39b
wellons <wellons@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-06-21 15:22:03 +0000

    Added seeker predictive shooting.
    git-svn-id: http://svn.nullprogram.com/matbots@5 8d8c6575-5c33-0410-b80b-87a992443ace

--- seeker.m
+++ seeker.m
@@ -1,3 +1,8 @@
+% Seeker bot
+% by Chris Wellons
+%
+% Selects a random target. Performs seek and destroy on that target until
+% it is dead.
 function [deltaH throttle action] = seeker(state, player, objects)
 
 pnum = player{6};
@@ -16,19 +21,31 @@ if exist(['seeker' num2str(pnum) '.mat'], 'file')
     if tnum == -1
         tnum = ceil(rand*length(state));
         target = state{tnum}{6};
+        lx = state{tnum}{1};
+        ly = state{tnum}{1};
     end
 else
     tnum = ceil(rand*length(state));
     target = state{tnum}{6};
     wt = 0;
+    lx = state{tnum}{1};
+    ly = state{tnum}{1};
 end
 
 tx = state{tnum}{1};
 ty = state{tnum}{2};
 
-thd = atan2(ypos - ty, xpos - tx);
+d = norm([tx-xpos ty-ypos]);
+px = (tx - lx)*2*d + tx;
+py = (ty - ly)*2*d + ty;
 
-if (abs(thd - hd) < pi/32)
+thd = atan2(ypos - py, xpos - px);
+
+% Note the targets current position for next time.
+lx = tx;
+ly = ty;
+
+if (abs(wrappi(thd - hd)) < pi/32)
     if  (wt > 20)
         action = 'rifle';
         wt = 0;
@@ -39,10 +56,14 @@ if (abs(thd - hd) < pi/32)
     throttle = 1;
 else
     wt = wt + 1;
-action = 'none';
+    action = 'none';
     throttle = 0.25;
 end
 
 deltaH = thd - hd;
+deltaH = wrappi(deltaH);
 
-save(['seeker' num2str(pnum)], 'target', 'wt');
+save(['seeker' num2str(pnum)], 'target', 'wt', 'lx', 'ly');
+
+function out = wrappi(in)
+out = mod(in + pi, 2*pi) - pi;
---
+++ zombie.m
@@ -0,0 +1,5 @@
+function [deltaH throttle action] = zombie(state, player, objects)
+
+deltaH = 0;
+action = 'none';
+throttle = 1;
