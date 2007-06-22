8d7ace506b86f8562f1207a977ff675257a95dc9
wellons <wellons@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-06-22 18:04:15 +0000

    Added minelayer bot.
    git-svn-id: http://svn.nullprogram.com/matbots@27 8d8c6575-5c33-0410-b80b-87a992443ace

---
+++ minelayer.m
@@ -0,0 +1,43 @@
+function [deltaH throttle action] = minelayer(state, player, objects)
+
+eval('engine_settings');
+
+% Handle matfile
+pnum = player{6};
+matfile = ['minelayer' num2str(pnum)];
+if isempty(state)
+    delete([matfile '.mat']);
+end
+if exist([matfile '.mat'], 'file')
+    load(matfile);
+else
+    wt = 0;
+    refill = 0;
+end
+
+[deltaH throttle action] = zombie(state, player, objects);
+
+if wt <= 0 && ~refill
+    action = 'mine';
+    throttle = 1;
+    wt = (mine_radius / ts) * 2;
+else
+    wt = wt - 1;
+    action = 'none';
+    throttle = 1;
+end
+
+if refill
+    throttle = rand;
+    deltaH = (rand * deltaH_max * 2) - deltaH_max;
+end
+
+if player{4} <= mine_cost
+    refill = 1;
+end
+
+if player{4} > (energy_max * 0.7)
+    refill = 0;
+end
+
+save(matfile, 'wt', 'refill');
