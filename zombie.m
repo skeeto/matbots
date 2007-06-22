e5bd1b5040483c6b1aedfd75072e408a175e98d8
wellons <wellons@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-06-22 14:54:55 +0000

    Zombies now turn around at walls.
    git-svn-id: http://svn.nullprogram.com/matbots@12 8d8c6575-5c33-0410-b80b-87a992443ace

--- zombie.m
+++ zombie.m
@@ -1,5 +1,24 @@
 function [deltaH throttle action] = zombie(state, player, objects)
 
+eval('engine_settings');
+
+px = player{1};
+py = player{2};
+buffer = .1;
+
 deltaH = 0;
 action = 'none';
 throttle = 1;
+
+if (px < world(1) + buffer)
+    deltaH = pi
+end
+if (px > world(2) - buffer)
+    deltaH = pi
+end
+if (py < world(3) + buffer)
+    deltaH = pi
+end
+if (py > world(4) - buffer)
+    deltaH = pi
+end
