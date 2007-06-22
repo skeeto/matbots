deaa547b97f3a652ce1aa3bafef63e66a8c3a02c
abraham <abraham@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-06-22 19:17:06 +0000

    Updated sniper.m
    git-svn-id: http://svn.nullprogram.com/matbots@32 8d8c6575-5c33-0410-b80b-87a992443ace

--- sniper.m
+++ sniper.m
@@ -1,5 +1,7 @@
 function [deltaH throttle action] = sniper(state,player,objects)
 
+engine_settings;
+
 num = player{6};
 
 datafile = ['sniper' num2str(num) '.mat'];
@@ -25,14 +27,6 @@ heading = player{8};
 
 nothers = size(state,2);
 
-%fprintf('I am: %d \n',num)
-%fprintf('I see %d other players.\n',nothers)
-%fprintf('They are: ')
-%for i = 1:nothers
-%    fprintf('%d ',state{i}{6})
-%end
-%fprintf('\n')
-
 dist = 10000;
 for i = 1:nothers
     if ~strcmp(team,state{i}{5})
@@ -43,13 +37,9 @@ for i = 1:nothers
     end
 end
 
-sweep = atan2(10/dist,dist);
-
 if target~=oldtarget
     firenumber = 0;
 end
-
-%fprintf('I am targeting %d \n',state{target}{6})
 
 targetx = state{target}{1};
 targety = state{target}{2};
@@ -58,27 +48,15 @@ targethist = [targethist; targetx targety];
 if size(targethist,1)==3
     targethist(1,:) = [];
     targetvector = targethist(2,:)-targethist(1,:);
-    timetotarget = dist/10;
+    timetotarget = dist/rifle_speed;
     newtarget=[targetx targety] + targetvector*(timetotarget/0.05);
     targetx = newtarget(1);
     targety = newtarget(2);
 end
 
-
-
-%fprintf('I am at [%f %f]\n',xpos, ypos)
-%fprintf('%d is at [%f %f]\n',state{target}{6},targetx,targety)
-
 aim = atan2(targety-ypos,targetx-xpos);
-
-%fprintf('My current heading is: %f deg\n',180/pi*heading)
-%fprintf('To hit %d I should aim at %f deg\n',state{target}{6},aim*180/pi)
-
 deltaH = aim-heading;
-
 deltaH = mod(deltaH+pi,2*pi)-pi;
-
-%fprintf('I am changing my heading by %f deg\n',deltaH*180/pi)
 
 if abs(deltaH)<pi/4
     if firenumber==0
@@ -96,7 +74,12 @@ if firenumber == 20;
    firenumber = 0;
 end
 
+if dist<.05
+    action = 'mine';
+end
+
 %pause
-throttle = 1;
+
+throttle = (dist-rifle_radius)^2;
 
 save (datafile,'firenumber','target','targethist')
