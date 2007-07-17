1e7a6a483576b84bf6357e639a73b5757692891b
abraham <abraham@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-07-17 20:30:19 +0000

    Fixed a bug in the preclean routine and also added firing logic to followers so they don't fire wild shots.
    git-svn-id: http://svn.nullprogram.com/matbots@74 8d8c6575-5c33-0410-b80b-87a992443ace

--- goose.m
+++ goose.m
@@ -5,16 +5,12 @@ engine_settings
 num = player{6};
 datafile = ['goosedat' player{5} '.mat'];
 
-if isempty(state)
-    if exist(datafile,'file')
-        delete (datafile)
-    end
-    return
-end
 
-if exist(datafile,'file') %if the .mat file exists
-    load(datafile)
-else  %initialize .mat file
+
+if strcmp(req,'preclean')||strcmp(req,'clean')
+    if exist(datafile,'file')
+        delete(datafile)
+    end
     linked = 0;
     dist2 = 100;
     dist3 = 100;
@@ -24,8 +20,11 @@ else  %initialize .mat file
     target = [];
     targethist = [];
     otherfile = [];
-
+    save (datafile,'linked','dist2','dist3','leaderaction','leaderheading','leaderthrottle','targethist','otherfile')
+    return
 end
+
+load (datafile)
 
 xpos = player{1};
 ypos = player{2};
@@ -40,7 +39,7 @@ nothers = size(state,2);
 
 gooselist = [];
 for i = 1:nothers
-    
+
     if strcmp(state{i}{7},'goose')&&strcmp(state{i}{5},team)&&(state{i}{3}>0)
         gooselist = [gooselist state{i}{6}];
     end
@@ -69,82 +68,88 @@ if myrank==1
     leaderaction = action;
     leaderheading = heading;
     leaderthrottle = throttle;
-    otherfile = ['sniper' num2str(num) '.mat'];
+    otherfile = ['sniper' team num2str(num) '.mat'];
     load (otherfile);
-    
+
 elseif mod(myrank,2)
     load (otherfile)
     theleader = gooselist(floor(myrank/2));
-    
+
     for i = 1:nothers
         if state{i}{6}==theleader
             whichstate = i;
         end
     end
-    
+
     action = leaderaction;
-     
+
     if strcmp(action,'rifle')
         dist = norm([state{target}{1}-xpos  state{target}{2}-ypos]);
-             
-            targetx = state{target}{1};
-            targety = state{target}{2};
-            targetvector = targethist(2,:)-targethist(1,:);
-            timetotarget = dist/rifle_speed;
-            newtarget=[targetx targety] + targetvector*(timetotarget/0.05);
-            targetx = newtarget(1);
-            targety = newtarget(2);
-        
+
+        targetx = state{target}{1};
+        targety = state{target}{2};
+        targetvector = targethist(2,:)-targethist(1,:);
+        timetotarget = dist/rifle_speed;
+        newtarget=[targetx targety] + targetvector*(timetotarget/0.05);
+        targetx = newtarget(1);
+        targety = newtarget(2);
+
         aim = atan2(targety-ypos,targetx-xpos);
         deltaH = aim-heading;
         deltaH = mod(deltaH+pi,2*pi)-pi;
         throttle = leaderthrottle;
+        if abs(deltaH)>deltaH_max
+            action = 'none';
+        end
     else
-      
-    RFvec = R*[cos(FlankAngle) sin(FlankAngle)]';
-    RFvec = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]*RFvec;
-    RFvec = [cos(-h) sin(-h); -sin(-h) cos(-h)]*RFvec;
-    RFpos = [state{whichstate}{1} state{whichstate}{2}]' + RFvec;
 
-    targetx = RFpos(1);
-    targety = RFpos(2);
+        RFvec = R*[cos(FlankAngle) sin(FlankAngle)]';
+        RFvec = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]*RFvec;
+        RFvec = [cos(-h) sin(-h); -sin(-h) cos(-h)]*RFvec;
+        RFpos = [state{whichstate}{1} state{whichstate}{2}]' + RFvec;
 
-    aim = atan2(targety-ypos,targetx-xpos);
-    deltaH = aim-heading;
-    deltaH = mod(deltaH+pi,2*pi)-pi;
-    dist2 = norm([targetx targety]-[xpos ypos]);
+        targetx = RFpos(1);
+        targety = RFpos(2);
+
+        aim = atan2(targety-ypos,targetx-xpos);
+        deltaH = aim-heading;
+        deltaH = mod(deltaH+pi,2*pi)-pi;
+        dist2 = norm([targetx targety]-[xpos ypos]);
 
 
-    throttle = dist2^4;
+        throttle = dist2^4;
     end
-    
+
 elseif ~mod(myrank,2)
-load (otherfile)
+    load (otherfile)
     theleader = gooselist(floor(myrank/2));
     for i = 1:nothers
         if state{i}{6}==theleader
             whichstate = i;
         end
     end
-    
+
     action = leaderaction;
 
     if strcmp(action,'rifle')
 
         dist = norm([state{target}{1}-xpos  state{target}{2}-ypos]);
-            
-            targetx = state{target}{1};
-            targety = state{target}{2};
-            targetvector = targethist(2,:)-targethist(1,:);
-            timetotarget = dist/rifle_speed;
-            newtarget=[targetx targety] + targetvector*(timetotarget/0.05);
-            targetx = newtarget(1);
-            targety = newtarget(2);
-        
-            aim = atan2(targety-ypos,targetx-xpos);
-            deltaH = aim-heading;
-            deltaH = mod(deltaH+pi,2*pi)-pi;
-            throttle = leaderthrottle;
+
+        targetx = state{target}{1};
+        targety = state{target}{2};
+        targetvector = targethist(2,:)-targethist(1,:);
+        timetotarget = dist/rifle_speed;
+        newtarget=[targetx targety] + targetvector*(timetotarget/0.05);
+        targetx = newtarget(1);
+        targety = newtarget(2);
+
+        aim = atan2(targety-ypos,targetx-xpos);
+        deltaH = aim-heading;
+        deltaH = mod(deltaH+pi,2*pi)-pi;
+        throttle = leaderthrottle;
+        if abs(deltaH)>deltaH_max
+            action = 'none';
+        end
     else
         LFvec = R*[-cos(FlankAngle) sin(FlankAngle)]';
         LFvec = [cos(pi/2) sin(pi/2); -sin(pi/2) cos(pi/2)]*LFvec;
@@ -159,7 +164,6 @@ load (otherfile)
         deltaH = mod(deltaH+pi,2*pi)-pi;
 
         dist3 = norm([targetx targety]-[xpos ypos]);
-
         throttle = dist3^4;
 
     end
