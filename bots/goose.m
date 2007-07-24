b465160bdda652c6413a834c52a5c70bf822890f
abraham <abraham@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-07-24 21:09:09 +0000

    Added documentation.  Goose now draws himself.
    git-svn-id: http://svn.nullprogram.com/matbots@123 8d8c6575-5c33-0410-b80b-87a992443ace

--- bots/goose.m
+++ bots/goose.m
@@ -1,28 +1,45 @@
 function [deltaH throttle action] = goose(state,player,objects,req)
+% GOOSE
+% coded by: Mike Abraham
+% last update: 07/19/2007
+%
+% This was my first bot with team cooperation capability.  I jumped
+% immediately into a large level of complexity.  The GOOSE bot travls in
+% formation with the rest of the geese on its team.  The GOOSE with the
+% highest player number is the leader and calls SNIPER.  The other geese
+% calculate their rank based upon their player number and task to a
+% specific point in a "flying V" type formation behind the lead bot.  The
+% follower geese will fire at the same target that the lead goose fires
+% upon IF they can turn quick enough.  If not, they don't waste energy on a
+% stray bullet.  The GOOSE bot will also quack occasionally.
 
 engine_settings
 
-num = player{6};
 datafile = ['goosedat' player{5} '.mat'];
 
-
-
-if strcmp(req,'preclean')||strcmp(req,'clean')
+if strcmp(req,'selfplot')
+    throttle = 0;
+    deltaH = 0;
+    action = req;
+    return
+elseif strcmp(req,'preclean')||strcmp(req,'clean')
     sniper(state,player,objects,req)
     if exist(datafile,'file')
         delete(datafile)
     end
     if strcmp(req,'preclean')
-    linked = 0;
-    dist2 = 100;
-    dist3 = 100;
-    leaderaction = 'none';
-    leaderheading = player{8};
-    leaderthrottle = [];
-    target = [];
-    targethist = [];
-    otherfile = [];
-    save (datafile,'linked','dist2','dist3','leaderaction','leaderheading','leaderthrottle','targethist','otherfile')
+        linked = 0;
+        dist2 = 100;
+        dist3 = 100;
+        leaderaction = 'none';
+        leaderheading = player{8};
+        leaderthrottle = [];
+        target = [];
+        targethist = [];
+        otherfile = [];
+        whoquacks = 0;
+        quackcount = 0;
+        save (datafile,'linked','dist2','dist3','leaderaction','leaderheading','leaderthrottle','targethist','otherfile','whoquacks','quackcount')
     end
     return
 end
@@ -34,15 +51,16 @@ ypos = player{2};
 health = player{3};
 energy = player{4};
 team = player{5};
-
+num = player{6};
 name = player{7};
 heading = player{8};
 h = heading;
+
 nothers = size(state,2);
 
+%% Identify and Sort All Friendly Geese
 gooselist = [];
 for i = 1:nothers
-
     if strcmp(state{i}{7},'goose')&&strcmp(state{i}{5},team)&&(state{i}{3}>0)
         gooselist = [gooselist state{i}{6}];
     end
@@ -51,6 +69,7 @@ end
 gooselist = [gooselist num];
 gooselist = sort(gooselist);
 
+%% Check for end of game condition
 if length(gooselist)==(nothers+1)
     action = 'none';
     throttle = 0;
@@ -58,14 +77,31 @@ if length(gooselist)==(nothers+1)
     return
 end
 
+%% Randomly Quack
+if quackcount==0
+    if rand<.01
+        quackcount = 10;
+        whoquacks = num;
+    end
+end
+
+if quackcount>0
+    if num==whoquacks
+        quackcount = quackcount-1;
+        text(xpos,ypos+.3,'quack')
+    end
+end
+
+%% Determine my rank
 myrank = find(gooselist==num);
 
+%% Bot Parameters
 R = rifle_radius*1.5; %Distance of flankers from leader
 FlankAngle = 70*pi/180; %Angle behind leader
-
 MDH = pi/25; %max delta H
 MT = 0.576; %max throttle
 
+%% Lead Goose Actions
 if myrank==1
     [deltaH throttle action] = sniper(state,player,objects,req);
     leaderaction = action;
@@ -74,6 +110,7 @@ if myrank==1
     otherfile = ['sniper' team num2str(num) '.mat'];
     load (otherfile);
 
+%% Right Flanker Actions
 elseif mod(myrank,2)
     load (otherfile)
     theleader = gooselist(floor(myrank/2));
@@ -119,10 +156,10 @@ elseif mod(myrank,2)
         deltaH = mod(deltaH+pi,2*pi)-pi;
         dist2 = norm([targetx targety]-[xpos ypos]);
 
-
         throttle = dist2^4;
     end
 
+%% Left Flanker Actions
 elseif ~mod(myrank,2)
     load (otherfile)
     theleader = gooselist(floor(myrank/2));
@@ -177,4 +214,16 @@ if (dist2<R*1.2)&&(dist3<R*1.2)
     linked = 1;
 end
 
-save (datafile,'linked','dist2','dist3','leaderaction','leaderheading','leaderthrottle','targethist','otherfile')
+xdraws = [.125 -.125 -.125/3 -.125 .125];
+ydraws = [0 .125 0 -.125 0];
+
+L = [ cos(heading+deltaH) -sin(heading+deltaH)  ; sin(heading+deltaH) cos(heading+deltaH)];
+P = L*[xdraws; ydraws];
+
+if abs(throttle)>1
+    throttle = throttle/abs(throttle);
+end
+
+eplot(xpos+ts*throttle*cos(heading+deltaH)+P(1,:),ypos+ts*throttle*sin(heading+deltaH)+P(2,:),'Color', player{9});
+
+save (datafile,'linked','dist2','dist3','leaderaction','leaderheading','leaderthrottle','targethist','otherfile','whoquacks','quackcount')
