06e1c380f91c3b93f0de4dae49762592b3bc65f5
wellons <wellons@8d8c6575-5c33-0410-b80b-87a992443ace>
2007-07-26 20:51:58 +0000

    Adjusted heading length setting - no longer scales to the world.
    git-svn-id: http://svn.nullprogram.com/matbots@136 8d8c6575-5c33-0410-b80b-87a992443ace

--- engine_settings.m
+++ engine_settings.m
@@ -9,7 +9,7 @@ group_teams_radius = norm([world(1)-world(2) world(3)-world(4)]) / 10;
 
 %--------------------------------------------------------------------------
 % Display settings
-heading_length = norm([world(1) - world(2) world(3) - world(4)])/35;
+heading_length = 0.4;
 display_game = 1;
 record_game = 0;
 script_game = 1;
